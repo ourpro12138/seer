1984d398b5d28053747ab91b8751dc12f6b82f10
乌无午雾 <31537825+az1315595523@users.noreply.github.com>
2023-01-12 20:14:25 +0800

    111

--- enemy.cpp
+++ enemy.cpp
@@ -2,9 +2,35 @@
 
 enemy::enemy()
 {
-
+    speedFactor=100;
+    setStatus=1;  nowStatus=1;
+    
 }
 enemy::~enemy()
 {
 
 }
+
+//QRectF enemy::boundingRect() const
+//{
+    
+//}
+
+//void enemy::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+//{
+//    Q_UNUSED(option);
+//    Q_UNUSED(Widget);
+     
+//}
+//void enemy::advance(int phase)
+//{
+//    if(!phase)
+//    return;
+//    update();
+    
+//}
+
+//void enemy::movemovie() 
+//{
+    
+//}
