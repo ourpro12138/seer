05573fa01903b7099033b2abf781fc22463181ca
Ikaros12138 <121995800+Ikaros12138@users.noreply.github.com>
2023-01-12 17:27:09 +0800

    123

--- enemy.h
+++ enemy.h
@@ -1,11 +1,34 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 #include "elf.h"
-
+#include<QGraphicsItem>
+#include<QPainter>
+#include<QRectF>
+#include<QMovie>
 class enemy: public Elf
 {
 public:
     enemy();
+    int atk;//攻击
+    int hp;//血
+    int attritube;//属性
+    int posX,posY;//位置
+    QString name;//名字
+    QMovie *movemovie;//移动动画
+    int speedFactor;
+    qreal speed;
+    int setStatus;
+    int nowStatus;
+
+
+
+
+//   QRectF boundingRect() const override;
+//    void paint(QPainter *painter,const QStyleOptionGraphicsItem *opition,QWidget *widget) override;
+//    void advance(int phase);
+//    bool collidesWithItem(const QGraphicsItem *other,Qt::ItemSelectionMode mode) const override;
+
+
     int benefit;
     ~enemy();
 };
